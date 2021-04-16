5b5a2bb220a3afc9f05852a02f856b87d5fd1de8
James Sturtevant <jstur@microsoft.com>
2021-04-16 15:08:32 -0700

    wired together

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+  -rg | --resource-group )
+    shift; resourcegroup=$1
+    ;;
+  -t | --gh-token )
+    shift; ghtoken=$1
+    ;;
+  -gh | --gh-account )
+    shift; ghaccount=$1
+    ;;
+  -l | --location )
+    shift; location=$1
+    ;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+
+location=${location:-westus2}
+resourcegroup=${resourcegroup:-"sig-windows-testgrid-metrics"}
+ghtoken=${ghtoken:?"github access token is required with workflow permissions"}
+ghaccount=${ghaccount:?"github account is required"}
+
+az group create --name $resourcegroup --location $location
+az staticwebapp create \
+    -n $resourcegroup \
+    -g $resourcegroup \
+    -s https://github.com/${ghaccount}/sig-windows-testgird-metrics \
+    -l $location \
+    -b main \
+    --app-artifact-location "dist" \
+    --token ${ghtoken}
--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,19 @@ export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      value: ""
     };
+  },
+  methods: {
+    getMetrics: function () {
+      fetch('/api/metrics')
+        .then(response => response.json())
+        .then(data => this.value = data.message);
+
+    }
+  },
+  mounted () {
+    this.getMetrics()
   }
 };
 </script>
